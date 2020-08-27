dac9910af87c863eb5b995aaaa8b3bdee2eabf1e
Akansha Jain <akansha.jn111@gmail.com>
2020-08-27 15:28:20 +0530

    Encapsulate live data

--- andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,16 +1,23 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GameViewModel : ViewModel(){
+class GameViewModel : ViewModel() {
 
     // The current word
-    var word = MutableLiveData<String>()
+    private var _word = MutableLiveData<String>()
 
     // The current score
-    var score = MutableLiveData<Int>()
+    private var _score = MutableLiveData<Int>()
+
+    val score: LiveData<Int>
+        get() = _score
+
+    val word: LiveData<String>
+        get() = _word
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
@@ -47,32 +54,32 @@ class GameViewModel : ViewModel(){
         if (wordList.isEmpty()) {
             //gameFinished()
         } else {
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
 
     }
 
-     fun onSkip() {
-        score.value= score.value?.minus(1)
+    fun onSkip() {
+        _score.value = score.value?.minus(1)
         nextWord()
     }
 
-     fun onCorrect() {
-        score.value= score.value?.plus(1)
+    fun onCorrect() {
+        _score.value = score.value?.plus(1)
         nextWord()
     }
 
 
-    init{
-        Log.i("GameViewModel","GameViewModel created!")
+    init {
+        Log.i("GameViewModel", "GameViewModel created!")
         resetList()
         nextWord()
-        score.value=0
+        _score.value = 0
     }
 
     override fun onCleared() {
         super.onCleared()
-        Log.i("GameViewModel","GameViewModel destroyed")
+        Log.i("GameViewModel", "GameViewModel destroyed")
     }
 
 }
